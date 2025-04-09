d2d64fa437a5a6f075b605dc63a7a7ba14da4105
kahramankaradavut <117511123+kahramankaradavut@users.noreply.github.com>
2025-04-09 14:52:41 +0300

    temp

--- MobileAppSersim/src/app/tab1/tab1.page.ts
+++ MobileAppSersim/src/app/tab1/tab1.page.ts
@@ -1,13 +1,67 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { PhotoService } from '../services/photo.service';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { camera } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { HttpClientModule } from '@angular/common/http'; 
+
+
 
 @Component({
+  standalone: true,
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [ExploreContainerComponent, IonicModule, FormsModule, CommonModule, HttpClientModule],
 })
 export class Tab1Page {
-  constructor() {}
+
+  selectedFile: File | null = null;
+
+  constructor(public photoService: PhotoService) { addIcons({ camera }); }
+
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  uploadPhoto() {
+    if (this.selectedFile) {
+      this.photoService.uploadImage(this.selectedFile).subscribe({
+        next: (res) => console.log('Başarılı:', res),
+        error: (err) => console.error('Hata:', err)
+      });
+    }
+  }
+
+
+
+
+
+
+
+
+  // serialNumber: string = '';
+  // userName: string = 'Kullanıcı Adı'; // Bu, giriş yapan kullanıcının ismi olmalı
+  // capturedPhotos: string[] = [];
+
+  // constructor(private photoService: PhotoService) {addIcons({ camera });}
+
+  // async capturePhoto() {
+  //   if (this.serialNumber.trim() === '') {
+  //     alert('Seri numarası boş olamaz!');
+  //     return;
+  //   }
+
+  //   try {
+  //     const savedFile = await this.photoService.capturePhoto(this.serialNumber, this.userName);
+  //     this.capturedPhotos.push(savedFile); // Fotoğrafı listeye ekle
+  //     console.log('Fotoğraf başarıyla kaydedildi:', savedFile);
+  //   } catch (error) {
+  //     console.error('Fotoğraf çekme hatası:', error);
+  //   }
+  // }
 }
--- MobileAppSersim/src/main.ts
+++ MobileAppSersim/src/main.ts
@@ -1,14 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideHttpClient(),
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
+defineCustomElements(window);
+
+
+
